36e97d35851769be46c2756b898d86627f8e764c
j3leonardodelgado <j3leonardodelgado@gmail.com>
2023-12-29 16:58:51 +0000

    vite config updated

--- src/SupabaseClient/client.js
+++ src/SupabaseClient/client.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { createClient } from "@supabase/supabase-js";
 const url = import.meta.env.VITE_URL_DATABASE
-const apiKey=import.meta.env.VITE_API_KEY || 
+const apiKey=import.meta.env.VITE_API_KEY 
   
 export const supabase = createClient(url, apiKey);
 
